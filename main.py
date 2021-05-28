3392795a7a2f1840a2c89d48b43834dd356f0782
Guilherme Balduino Lopes <73725712+GuilhermeMikin@users.noreply.github.com>
2021-05-28 13:46:36 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from clientModbus import ClienteMODBUS
 
 dbpath = "C:\\Users\\Guilherme B. Lopes\\Documents\\GitHub\\EnergyEfficiencyproject\\DB\\database.db"
 
-c = ClienteMODBUS('192.168.1.5', 502, dbpath=dbpath)
+c = ClienteMODBUS('localhost', 502, dbpath=dbpath)
 c.atendimento()
 
 
